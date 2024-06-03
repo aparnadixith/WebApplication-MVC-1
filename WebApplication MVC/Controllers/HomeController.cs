2ca07797f0c8b9b065bb69b1d75d54443723ef94
Aparna <aparnadixith07@gmail.com>
2024-06-03 22:49:34 +0200

    Add project files.

---
+++ WebApplication MVC/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Title"] = "About";
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Title"] = "Contact";
+            return View();
+        }
+
+        public IActionResult Projects()
+        {
+            ViewData["Title"] = "Projects";
+            return View();
+        }
+    }
+}
+
